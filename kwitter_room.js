5c067663eafc74007505dad50610d142b404d765
clank <84193593+clank-bot@users.noreply.github.com>
2021-10-16 13:04:26 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,48 @@
+
+//ADD YOUR FIREBASE LINKS HERE
+const firebaseConfig = {
+      apiKey: "AIzaSyABl4XoxmemEUEOylxPrSvbNGmByfwRJhw",
+      authDomain: "game-412e2.firebaseapp.com",
+      databaseURL: "https://project-prototype-7cf53-default-rtdb.firebaseio.com",
+      projectId: "game-412e2",
+      storageBucket: "game-412e2.appspot.com",
+      messagingSenderId: "213792653737",
+      appId: "1:213792653737:web:0d3996f0c0f3c6be67c4b8"
+    };
+    
+    firebase.initializeApp(firebaseConfig);
+
+    user_name = localStorage.getItem("user_name")
+
+    document.getElementById("user_name").innerHTML = "Welcome " + user_name + "!";
+
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Start code
+      console.log("Room Name - " + Room_names);
+      row = "<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)' >#"+ Room_names +"</div><hr>";
+      document.getElementById("output").innerHTML += row;
+      //End code
+      });});}
+getData();
+
+function logout()
+{
+localStorage.removeItem("user_name");
+localStorage.removeItem("room_name");
+    window.location = "index.html"
+}
+
+function addRoom()
+{
+      add_Room = document.getElementById("room_name").value;
+
+      firebase.database().ref("/").child(add_Room).update({
+            purpose : "adding room name"
+      });
+
+      localStorage.setItem("room_name", add_Room);
+
+      window.location = "kwitter_page.html";
+}
